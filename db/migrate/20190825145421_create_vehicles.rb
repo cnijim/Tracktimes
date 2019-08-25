a319771dfe6ffab320c4ba7b8a8a70b34415f2ff
Chad Nijim <cnijim@gmail.com>
2019-08-25 10:07:47 -0500

    vehicle model added

--- db/migrate/20190825145421_create_vehicles.rb
+++ db/migrate/20190825145421_create_vehicles.rb
@@ -1,8 +1,12 @@
 class CreateVehicles < ActiveRecord::Migration[5.2]
   def change
     create_table :vehicles do |t|
-
+      t.integer :user_id
+      t.integer :year
+      t.string :make
+      t.string :model
       t.timestamps
     end
+    add_index :vehicles, :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_042905) do
+ActiveRecord::Schema.define(version: 2019_08_25_145421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2019_08_23_042905) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "year"
+    t.string "make"
+    t.string "model"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
 end
